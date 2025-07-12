72ec502781b1996746fbb653f849c572160a3884
BenTachie <tachiebenedict@gmail.com>
2025-07-12 15:49:56 +0200

    Add Teacher and Director interfaces with example objects

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,3 +7,30 @@ interface Teacher {
   location: string; // A required string. Must be defined.
   [key: string]: any // This allows for additional properties of any type.
 }
+
+// This is a Teacher object
+const teacher1: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false, 
+};
+
+console.log(teacher1);
+
+// This interface is for a director, which extends the Teacher interface
+interface Director extends Teacher {
+  numberOfReports: number; // A required number indicating how many teachers reports the director has
+}
+
+// This is a Director object
+const director1: Director = {
+  firstName: 'Jane',
+  fullTimeEmployee: true,
+  lastName: 'Smith',
+  location: 'New York',
+  numberOfReports: 5,
+};
+
+console.log(director1);
